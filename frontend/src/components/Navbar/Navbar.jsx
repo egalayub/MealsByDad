ba94027eb9b227d6439f43695383a8c1c133997b
Ryan <ryangernert92@gmail.com>
2023-04-03 10:31:19 -0400

    login form and sign in

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -3,46 +3,52 @@ import { GiHamburgerMenu } from 'react-icons/gi';
 import { MdOutlineRestaurantMenu } from 'react-icons/md';
 import './navbar.css';
 import './logo.png';
+import LoginForm from "./LoginForm";
+import SignSignInButton from "./SignInButton";
+import SignInButton from './SignInButton';
 
 const Navbar = () => {
 
-    const[toggleMenu, setToggleMenu] = useState(false);
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [isShowLogin, setIsShowLogin] = useState(true);
+  const handleLoginClick = () => {
+    setIsShowLogin((isShowLogin) => !isShowLogin);
+  };
 
-    
+
   return (
-    <nav className="app_navbar">
-        <div className='app__navbar-logo'>
+    <nav className="navbar">
+      <div className='app__navbar-logo'>
         <img src="https://images.cooltext.com/5650975.png" alt="logo" />
-        </div>
-        <ul className='app__navbar-links'>
-            <li className='p__opensans'><a href="#home">Home</a></li>
-            <li className='p__opensans'><a href="#recipes">Find Recipes</a></li>
-            <li className='p__opensans'><a href="#resources">Resources</a></li>
-            <li className='p__opensans'><a href="#contact">Contact</a></li>
-        </ul>
-        <div className='app__navbar-login'>
-            <a href="#login" className='p_opensans'>Login / Register</a>
-            <div />
-            <a href="/" className='p_opensans'>Your Recipes</a>
-        </div>
-        <div className='app__navbar-smallscreen'>
-            <GiHamburgerMenu color='#fff' fontSize={27} onClick={() => setToggleMenu(true)} />
+      </div>
+      <ul className='app__navbar-links'>
+        <li className='p__opensans'><a href="#home">Home</a></li>
+        <li className='p__opensans'><a href="#recipes">Find Recipes</a></li>
+        <li className='p__opensans'><a href="#resources">Resources</a></li>
+         <li className='p__opensans'><a href="#contact">Contact</a></li>
+      </ul>
+      <div className="navbar">
+      <SignInButton handleLoginClick={handleLoginClick} />
+      <LoginForm isShowLogin={isShowLogin} />
+    </div>
+{/*       <div className='app__navbar-smallscreen'> */}
+{/*         <GiHamburgerMenu color='#fff' fontSize={27} onClick={() => setToggleMenu(true)} /> */}
 
-            {toggleMenu && (
-          <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
-            <MdOutlineRestaurantMenu className='overlay__close'  fontSize={27} onClick={() => setToggleMenu(false)} />
-            <ul className='app__nav-smallscreen_links'>
-                <li className='p__opensans'><a href="#home">Home</a></li>
-                <li className='p__opensans'><a href="#recipe">Find Recipe</a></li>
-                <li className='p__opensans'><a href="#resources">Resources</a></li>
-                <li className='p__opensans'><a href="#contact">Contact</a></li>
-        </ul>   
-            </div>
-            )}
-            
+{/*         {toggleMenu && ( */}
+{/*           <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'> */}
+{/*             <MdOutlineRestaurantMenu className='overlay__close' fontSize={27} onClick={() => setToggleMenu(false)} /> */}
+{/*             <ul className='app__nav-smallscreen_links'> */}
+{/*               <li className='p__opensans'><a href="#home">Home</a></li> */}
+{/*               <li className='p__opensans'><a href="#recipe">Find Recipe</a></li> */}
+{/*               <li className='p__opensans'><a href="#resources">Resources</a></li> */}
+{/*               <li className='p__opensans'><a href="#contact">Contact</a></li> */}
+{/*             </ul> */}
+{/*           </div> */}
+{/*         )} */}
 
 
-        </div>
+
+{/*       </div> */}
     </nav>
   )
 }
